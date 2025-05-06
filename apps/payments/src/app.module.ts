bfbca703354062a7bc7f4f46b79fa88703a9317f
Farias <farias.softwares@gmail.com>
2025-05-06 05:23:09 -0300

    feat: add queue system with bull

--- apps/payments/src/app.module.ts
+++ apps/payments/src/app.module.ts
@@ -4,12 +4,20 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { DepositModule } from './deposit/deposit.module'
 import { WalletModule } from './wallet/wallet.module'
+import { BullModule } from '@nestjs/bullmq'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: path.resolve(__dirname, '../../../.env'),
       isGlobal: true,
+    }),
+    BullModule.forRoot({
+      connection: {
+        host: process.env.DB_REDIS_HOST,
+        port: Number(process.env.DB_REDIS_PORT),
+        password: process.env.DB_REDIS_PASSWORD,
+      }
     }),
     DepositModule,
     WalletModule
---
+++ apps/payments/src/config/bull.ts
@@ -0,0 +1,23 @@
+import { JobsOptions } from "bullmq"
+
+export const defaultJobOptions: JobsOptions = {
+  attempts: 5,
+  backoff: {
+    type: 'exponential',
+    delay: 2000,
+  },
+  lifo: false,
+  stackTraceLimit: 10,
+  removeOnComplete: false,
+  removeOnFail: false,
+}
+
+export const queueNames = {
+  deposit: "deposit"
+} as const
+
+export const jobNames = {
+  deposit: {
+    create: "create"
+  }
+} as const
