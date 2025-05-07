21c5fbf1886f28ba8e7f2f1aedefbc02875311f8
Farias <farias.softwares@gmail.com>
2025-05-07 20:02:21 -0300

    fix: remove unused auth provider.

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { AuthContextProvider } from '@/providers/auth'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from '@/providers/theme'
 import { SidebarProvider } from "@/components/layouts/sidebar"
@@ -8,29 +7,27 @@ import { ForceTheme } from "@/components/force-theme"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AuthContextProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem={false}
-      >
-        <SidebarProvider>
-          <Toaster
-            position="bottom-center"
-            toastOptions={{
-              duration: 5000,
-              style: {
-                width: 'auto',
-                maxWidth: '90dvw',
-                padding: "0.75rem 1.25rem",
-                borderRadius: '10px'
-              }
-            }}
-          />
-          {children}
-          <ForceTheme theme="dark" />
-        </SidebarProvider>
-      </ThemeProvider>
-    </AuthContextProvider>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem={false}
+    >
+      <SidebarProvider>
+        <Toaster
+          position="bottom-center"
+          toastOptions={{
+            duration: 5000,
+            style: {
+              width: 'auto',
+              maxWidth: '90dvw',
+              padding: "0.75rem 1.25rem",
+              borderRadius: '10px'
+            }
+          }}
+        />
+        {children}
+        <ForceTheme theme="dark" />
+      </SidebarProvider>
+    </ThemeProvider>
   )
 }
